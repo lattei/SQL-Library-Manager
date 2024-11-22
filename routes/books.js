d9e069e6610810e9ce9d8a33179c1281c93c3fd7
Carline Senat <carlinesenat@gmail.com>
2024-11-22 13:06:03 -0800

    Creating routes and pugs
    Creating pug layouts based on example markup

--- routes/books.js
+++ routes/books.js
@@ -3,10 +3,43 @@ const router = express.Router();
 const Book = require("../models").Book;
 const { Op } = require("sequelize");
 
+/* Handler function to wrap each route with */
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+function asyncHandler(cb) {
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      //Forward this to the global error handler!
+      next(error);
+    }
+  }
+}
+
+/* GET */
+router.get('/new', (req, res) => {
+  res.render('new-book', { title: "New Book" });
 });
+
+/* CREATE  / Post Books
+// Route: get /books/new - Shows the create new book form
+// View: new-book.pug - for the new book form
+// Create Book Route */
+router.post('/new', asyncHandler(async(req, res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books/" + book.id);
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      book = await Book.build(req.body);
+      res.render("new-book", { book, errors: error.errors})
+    } else {
+      throw error;
+    }
+  }
+}));
+
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page, redirect to Books. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/books');
 });
 
 module.exports = router;
